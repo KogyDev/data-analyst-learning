cfaa324f904ea5b5cad52eab4649d020451c7e75
Matheus Cornedi <matheus.r.cornedi@gmail.com>
2024-11-27 19:37:13 -0300

    Create Strings 05.py

---
+++ python_exercises/Strings 05.py
@@ -0,0 +1,7 @@
+# Entradas do usuário
+primeiro_nome = input("Digite seu primeiro nome: ")
+ultimo_nome = input("Digite seu último nome: ")
+
+# Concatenar e exibir mensagem
+nome_completo = primeiro_nome + " " + ultimo_nome
+print("Olá, " + nome_completo + "! Seja bem-vindo ao nosso programa.")
