22d132289af3056c282d9c61d5935870b3274ad8
Matheus Cornedi <matheus.r.cornedi@gmail.com>
2024-12-02 20:10:28 -0300

    Create Lista 04.py

---
+++ python_exercises/Lista 04.py
@@ -0,0 +1,10 @@
+# Exercício 1: Lista de Frutas
+# Objetivo: Manipular uma lista de frutas simples
+
+frutas = ["banana", "maçã", "laranja", "uva"]
+
+# Adicione uma nova fruta no final da lista
+frutas.append("morango")
+
+# Imprima o nome da terceira fruta da lista
+print("A terceira fruta é:", frutas[2])
