8e29b9bb3950a0b3694adc341284cb0b578ff3a5
Matheus Cornedi <matheus.r.cornedi@gmail.com>
2024-12-02 20:18:57 -0300

    Create Lista 05.py

---
+++ python_exercises/Lista 05.py
@@ -0,0 +1,10 @@
+# Exercício 2: Lista de Notas
+# Objetivo: Calcular a média de notas de um aluno
+
+notas = [7.5, 8.0, 6.5, 9.0]
+
+# Calcule a média das notas
+soma_notas = sum(notas)
+media = soma_notas / len(notas)
+
+print("Média das notas:", media)
