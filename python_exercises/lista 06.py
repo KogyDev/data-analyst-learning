feb7f929147be6f1e31dc44cd1a2657fb1a24c81
Matheus Cornedi <matheus.r.cornedi@gmail.com>
2024-12-02 20:20:34 -0300

    Create lista 06.py

---
+++ python_exercises/lista 06.py
@@ -0,0 +1,11 @@
+# Exercício 3: Lista de Idades
+# Objetivo: Encontrar a maior e menor idade
+
+idades = [25, 19, 42, 31, 16, 50]
+
+# Encontre a maior e menor idade
+idade_maxima = max(idades)
+idade_minima = min(idades)
+
+print("Maior idade:", idade_maxima)
+print("Menor idade:", idade_minima)
